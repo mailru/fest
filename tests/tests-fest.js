3eea3db428814e72d22bb3960e95068ffcf497d8
Andrew Sumin <AndrewSumin@gmail.com>
2012-01-19 20:49:50 +0400

    bug fix

--- tests/tests-fest.js
+++ tests/tests-fest.js
@@ -117,7 +117,7 @@ vows.describe('Fast tests').addBatch({
             return promise;
         },
         'result':function(result){
-            assert.equal(result, '<style>.foo{ont: 18px/18px "Helvetica Neue", Arial;}\n.bar{}</style>');
+            assert.equal(result, '<style>.foo{ont: 18px/18px "Helvetica Neue", Arial;}\n.bar{\\"\\"}</style>');
         }
     },
     'use strict':{
