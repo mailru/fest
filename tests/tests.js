f85b2863d5c74fa8850f92901c1e0b3d3bb3341e
Andrew Sumin <AndrewSumin@gmail.com>
2012-07-27 13:40:12 +0400

    escape fest:value inside script by default and update changelog

--- tests/tests.js
+++ tests/tests.js
@@ -1,6 +1,5 @@
 var fs = require('fs');
-
-fs.readdir(process.env.PWD, function(err, files){
+fs.readdir(process.argv[1].replace(/tests.js$/, ''), function(err, files){
     files.filter(function(file){return file.indexOf('tests-') === 0;})
          .forEach(function(file){
              require('./' + file);
