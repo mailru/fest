84f810b088ee9e87016afc07c8392c3607756033
Stanislav Tugovikov <s.tugovikov@corp.mail.ru>
2012-09-17 16:34:55 +0400

    replace spaces to tabs

--- tests/tests-shorttag.js
+++ tests/tests-shorttag.js
@@ -1,18 +1,18 @@
 var vows = require('vows'),
-    events = require('events'),
-    assert = require('assert'),
-    transform = require('./transform');
+	events = require('events'),
+	assert = require('assert'),
+	transform = require('./transform');
 
 vows.describe('Fest tests').addBatch({
-    'shorttag':{
-        topic:function(){
-            var promise = new(events.EventEmitter);
-            transform('/templates/shorttag.xml', {}, {}, promise);
-            return promise;
-        },
-        'result':function(result){
-            assert.equal(result, '<meta/>');
-        }
-    }
+	'shorttag':{
+		topic:function(){
+			var promise = new(events.EventEmitter);
+			transform('/templates/shorttag.xml', {}, {}, promise);
+			return promise;
+		},
+		'result':function(result){
+			assert.equal(result, '<meta/>');
+		}
+	}
 }).run();
 
--- tests/tests.js
+++ tests/tests.js
@@ -4,20 +4,20 @@ var sys = require('sys')
 var spawn = require('child_process').spawn;
 var children = [];
 fs.readdir(dir, function(err, files){
-    files.filter(function(file){return file.indexOf('tests-') === 0;})
-         .forEach(function(file, index){
-             var string = '',
-                 child = spawn('node', [dir + file]);
-             children.push(file);
-             child.stdout.on('data', function(data){
-                 string += data;
-             })
-             child.on('exit', function(){
-                 console.log(file, string.trim());
-                 children.splice(children.indexOf(file), 1);
-                 if (children.length === 0){
-                     process.exit();
-                 }
-             })
-         });
+	files.filter(function(file){return file.indexOf('tests-') === 0;})
+		 .forEach(function(file, index){
+			 var string = '',
+				 child = spawn('node', [dir + file]);
+			 children.push(file);
+			 child.stdout.on('data', function(data){
+				 string += data;
+			 })
+			 child.on('exit', function(){
+				 console.log(file, string.trim());
+				 children.splice(children.indexOf(file), 1);
+				 if (children.length === 0){
+					 process.exit();
+				 }
+			 })
+		 });
 });
