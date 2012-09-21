9abc6641100d925c9e64406e3918f64fb57bcfa5
Andrew Sumin <AndrewSumin@gmail.com>
2012-09-21 20:18:23 +0400

    refactoring tests.js

--- tests/tests.js
+++ tests/tests.js
@@ -3,6 +3,7 @@ var dir = process.argv[1].replace(/tests.js$/, '')
 var sys = require('sys')
 var spawn = require('child_process').spawn;
 var children = [];
+var commands = [];
 
 var modes = ['function', 'string', 'array'],
 	mode = process.argv[2];
@@ -11,23 +12,28 @@ if (mode && modes.indexOf(mode) > -1) {
 	modes = [mode];
 }
 
-fs.readdir(dir, function(err, files){
-	files.filter(function(file){return file.indexOf('tests-') === 0;})
-		.forEach(function(file, index){
-			modes.forEach(function (mode) {
-				var string = '',
-					child = spawn('node', [dir + file, mode]);
-				children.push(file);
-				child.stdout.on('data', function(data){
-					string += data;
-				})
-				child.on('exit', function(){
-					console.log(file + ' [' + mode + ']', string.trim());
-					children.splice(children.indexOf(file), 1);
-					if (children.length === 0){
-						process.exit();
-					}
-				})
-			})
-		 });
-});
+fs.readdirSync(dir).filter(function(file){return file.indexOf('tests-') === 0;})
+					.forEach(function(file, index){
+						modes.forEach(function(mode) {
+							commands.push({path:dir + file, mode:mode, file:file});
+						});
+					 });
+
+function next(command){
+	var string = '',
+		child = spawn('node', [command.path, command.mode]);
+	children.push(command.file);
+	child.stdout.on('data', function(data){
+		string += data;
+	})
+	child.on('exit', function(){
+		console.log(command.file + ' [' + command.mode + ']', string.trim());
+		if (commands.length === 0){
+			process.exit();
+		} else {
+			setTimeout(function(){next(commands.shift());}, 100)
+		}
+	})
+}
+
+next(commands.shift());
