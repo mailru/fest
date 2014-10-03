6084baaa36224d03d68c62dc8ed305151c47de2e
RubaXa <ibnRubaXa@gmail.com>
2014-10-03 19:29:34 +0400

    + Комментарии к bem:block

--- spec/bem:block.spec.js
+++ spec/bem:block.spec.js
@@ -5,7 +5,15 @@ describe('bem:block', function () {
     beforeEach(utils.setupMatchers);
 
     it('Объявление блока', function () {
-        expect(render('templates/bem:block.xml').contents).toBe('<script><![CDATA[alert ("2" < 3);]]></script>');
+        expect(render('templates/bem:block.xml').contents).toBe('' +
+			'<div class=" btn btn_main ">' +
+			'<span class="btn__text ">foo</span>' +
+			'</div>' +
+
+			'<div class=" btn  btn_icon ">' +
+			'<span class="btn__text  btn__text_pad">bar</span>' +
+			'</div>'
+		);
     });
 
 });
